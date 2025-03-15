674585c380c82b3c141f0fa088fb957da45413e3
Ayodtrader12 <ibrahimpopoola292@gmail.com>
2025-03-15 15:28:06 -0700

    second commit

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,11 +1,53 @@
+import React from 'react'
+import image from "../assets/team-sect.jpeg"
+
 const HomeScreen = () => {
   return (
-    <div className="relative min-h-[calc(100vh-140px)] bg-stone-100 border rounded-md p-2 ">
-      <div>start here</div>
-
-      <div>/bv/jb </div>
+    <div >
+      {/* hero page */}
+      <div className='w-full h-[90vh]'>
+        <div style={{
+          height:"100%",
+          width:"100%",
+          backgroundImage:`url(${image})`,
+          backgroundRepeat:'no-repeat'
+          , backgroundSize:'cover',
+          backgroundPosition:''
+        }}>
+          <div className='bg-black opacity-50 w-full h-full flex items-center'>
+            <div className='w-[40%] h-[70%]  ml-36'>
+              <h1 className='text-[70px] font-serif text-white'>
+                Summer Clearance Events Extra
+              </h1>
+              <p className='text-[80px] font-serif text-[#fbb5a4]'>
+               30% OFF
+              </p>
+              <p className='mt-7 text-white font-sans text-[25px]'>Up to 80% off sitewide with items as low as $5.99. While items last.
+              </p>
+              <button className='text-lg font-bold text-white bg-[#f75159] rounded-full px-10 py-3 mt-7 uppercase h-[65px]'>
+                shop now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* new product section */}
+      <div className='w-full h-[18vh]  flex items-end justify-center'>
+        <div className='w-[8%] h-[40%]  flex gap-3 flex-col items-center'>
+        <div className='w-[40%] h-[1px] bg-[#e3b0c2]'></div>
+                        <div className='text-[#e3b0c2] font-semibold text-center text-[15px]'>NEW PRODUCT</div>
+                        <div className='w-[40%] h-[1px] bg-[#e3b0c2]'></div>
+        </div>
+      </div>
+      <div className='w-full h-[25vh] '>
+        <p className='font-serif text-[70px] text-center '>Shop New Hijabs</p>
+        <p className='font-sans text-[20px] text-center mt-2 text-gray-500'>Lorem ipsum dolor sit amet consec contes quisque urna molestie tincidunt aliquet<br/> quam. Imperdiet at pellent esque vitae aliquet tempor eget rutrum tellus.</p>
+      </div>
+      <div className='w-full h-[50vh] bg-slate-400 flex justify-center'>
+        <div className='w-[80%] h-full bg-slate-50'></div>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default HomeScreen;
+export default HomeScreen
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import pix from "../assets/login.png"
+import pix1 from "../assets/logo-jamila.png"
 const login = () => {
   return (
     <div>
@@ -9,8 +10,33 @@ const login = () => {
                     <img src={pix} alt="image" className='h-[99%]' />
                 </div>
             </div>
-            <div className='w-[45%] h-full bg-slate-600 flex items-center justify-center'>
-              <div className='w-[90%] h-[90%] bg-slate-50'></div>
+            <div className='w-[45%] h-full  flex items-center justify-center'>
+              <div className='w-[90%] h-[90%] '>
+                <div className='h-[10%] w-full flex items-center justify-center mb-16'>
+                    <img src={pix1} alt="image" className='h-[90%]' />
+                </div>
+                <div className='w-full h-[15%]  flex items-center justify-center'>
+                    <div className='w-[20%] h-[75%] flex gap-3 flex-col items-center'>
+                        <div className='w-[50%] h-[1px] bg-[#e3b0c2]'></div>
+                        <div className='text-[#e3b0c2] font-semibold text-center'>LOGIN</div>
+                        <div className='w-[50%] h-[1px] bg-[#e3b0c2]'></div>
+                    </div>
+                    
+                </div>
+                <h1 className='text-[60px] font-serif text-center'>Welcome Back!</h1>
+                <p className='text-center mt-5 font-sans text-[20px]'>please enter your details</p>
+                <div className='w-full h-[25%]  flex items-center justify-center mt-5'>
+                    <div className='w-[75%] h-[80%] '>
+                        <input type="text"  className='bg-slate-200 border outline-none h-[55px] rounded-md w-full p-2 mb-4'/>
+                        <input type="text"  className='bg-slate-200 border outline-none h-[55px] rounded-md w-full p-2'/>
+                    </div>
+                </div>
+                <div className='w-full h-[15%]  flex items-center justify-center'>
+                    <button className='text-white font-bold bg-[#e4989c] h-[85px] w-[550px] rounded-full'>LOG IN</button>
+                </div>
+                <p className='text-[#e4989c] font-semibold ml-20 cursor-pointer
+                '>Lost your password?</p>
+              </div>
             </div>
         </div>
     </div>
